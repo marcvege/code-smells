18be73d363aaad095b098f8d52204bd346458f92
Marc Villagrasa <marcvege@gmail.com>
2017-07-11 16:32:43 +0200

    Change primitive with object Amount

---
+++ src/main/java/es/rachelcarmena/bloaters/smell1/Amount.java
@@ -0,0 +1,10 @@
+package es.rachelcarmena.bloaters.smell1;
+
+public class Amount {
+    private final double value;
+
+    public Amount(double value) {
+
+        this.value = value;
+    }
+}
--- src/test/java/es/rachelcarmena/bloaters/smell1/WithdrawShould.java
+++ src/test/java/es/rachelcarmena/bloaters/smell1/WithdrawShould.java
@@ -2,17 +2,15 @@ package es.rachelcarmena.bloaters.smell1;
 
 import org.junit.Test;
 
-import java.math.BigDecimal;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class WithdrawShould {
 
     @Test
-    public void keep_the_amount() {
-        Withdraw withdraw = new Withdraw(560d);
-
-        assertThat(withdraw.getAmount(), is(560d));
+    public void keep_the_amount_object() throws Exception {
+        Amount amount = new Amount(560d);
+        Withdraw withdraw = new Withdraw(amount);
+        assertThat(withdraw.getAmount(), is(amount));
     }
 }
